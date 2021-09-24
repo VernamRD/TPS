7d034e25a882ac2a44845731f0d83db4ebb320e7
VernamRD <vernamrdcross@mail.ru>
2021-09-25 00:43:24 +0300

    Build information

--- Source/TPS.Target.cs
+++ Source/TPS.Target.cs
@@ -2,11 +2,13 @@
 
 using UnrealBuildTool;
 using System.Collections.Generic;
+using System;
 
 public class TPSTarget : TargetRules
 {
     public TPSTarget(TargetInfo Target) : base(Target)
     {
+        Console.WriteLine("TPS game target ------------>");
         Type = TargetType.Game;
         DefaultBuildSettings = BuildSettingsVersion.V2;
         ExtraModuleNames.Add("TPS");
--- Source/TPSEditor.Target.cs
+++ Source/TPSEditor.Target.cs
@@ -2,11 +2,13 @@
 
 using UnrealBuildTool;
 using System.Collections.Generic;
+using System;
 
 public class TPSEditorTarget : TargetRules
 {
     public TPSEditorTarget(TargetInfo Target) : base(Target)
     {
+        Console.WriteLine("TPS game target ------------>");
         Type = TargetType.Editor;
         DefaultBuildSettings = BuildSettingsVersion.V2;
         ExtraModuleNames.Add("TPS");
