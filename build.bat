0ee5ea481c20cfdc53ddaaa584589451144fc16d
VernamRD <vernamrdcross@mail.ru>
2021-09-25 18:45:37 +0300

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,6 @@
+"E:\GameDev\UE4\UE_4.27\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project="E:\UE Projects\TPS\TPS.uproject" ^
+-platform=Win64 -clientconfig=Development ^
+-archivedirectory="E:\UE Projects\TPS\BuildFromBat" ^
+-build -cook -stage -skippackage -archive -pak -package
