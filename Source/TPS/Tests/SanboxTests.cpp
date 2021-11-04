1f4f8a6e0bcc40f5e6d95ef9c98dd79fcb368448
VernamRD <vernamrdcross@mail.ru>
2021-11-04 20:31:19 +0300

    Add sin func test and some refafctoring

--- Source/TPS/Tests/SanboxTests.cpp
+++ Source/TPS/Tests/SanboxTests.cpp
@@ -3,28 +3,43 @@
 #if (WITH_DEV_AUTOMATION_TESTS || WITH_PERF_AUTOMATION_TESTS)
 
 #include "Tests/SanboxTests.h"
+#include "Tests/TestUtils.h"
 #include "CoreMinimal.h"
 #include "Misc/AutomationTest.h"
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FMathMaxInt, "TPSGame.Math.MaxInt",
-                                 EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter |
-                                     EAutomationTestFlags::HighPriority);
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FMathSqrt, "TPSGame.Math.Sqrt",
-                                 EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter |
-                                     EAutomationTestFlags::HighPriority);
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FMathSin, "TPSGame.Math.Sin",
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
+
+using namespace TPSGame;
 
 bool FMathMaxInt::RunTest(const FString& Parameters)
 {
-    AddInfo("Max [int] func testing");
-    TestTrue("2 different positive numbers", FMath::Max(13, 25) == 25);
-    TestTrue("2 equal positive", FMath::Max(25, 25) == 25);
-    TestTrue("zero and positive", FMath::Max(0, 123) == 123);
-    TestTrue("2 zeros", FMath::Max(0, 0) == 0);
-    TestTrue("zero and negative", FMath::Max(-2351, 0) == 0);
-    TestTrue("2 different negative numbers", FMath::Max(-25, -74) == -25);
-    TestTrue("2 equal negative numbers", FMath::Max(-9, -9) == -9);
-    TestTrue("negative and positive numbers", FMath::Max(9, -9) == 9);
+
+    typedef TArray<TPSGame::TestPayLoad<TInterval<int32>, int32>> MaxIntTestPayLoad;
+    // clang-format off
+    const MaxIntTestPayLoad TestData 
+    {
+        {{13, 25}, 25},
+        {{25, 25}, 25},
+        {{0, 123}, 123},
+        {{0, 0}, 0},
+        {{-2345, 0}, 0},
+        {{-45, -67}, -45},
+        {{9, -9}, 9},
+
+    };
+    // clang-format on
+
+    for (const auto Data : TestData)
+    {
+        TestTrueExpr(FMath::Max(Data.TestValue.Min, Data.TestValue.Max) == Data.ExpectedValue);
+    }
 
     return true;
 }
@@ -35,11 +50,51 @@ bool FMathSqrt::RunTest(const FString& Parameters)
 
     AddInfo("Sqrt function testing");
 
-    TestEqual("Sqrt (4) [0]", FMath::Sqrt(4.0f), 2.0f);
-    // TestEqual("Sqrt (3) [1]", FMath::Sqrt(3.0f), 1.7f);
-    TestEqual("Sqrt (3) [2]", FMath::Sqrt(3.0f), 1.7f, 0.1f);
-    TestEqual("Sqrt (3) [3]", FMath::Sqrt(3.0f), 1.73f, 0.01f);
-    TestEqual("Sqrt (3) [4]", FMath::Sqrt(3.0f), 1.73205f, 1.e-5f);
+    typedef TArray<TestPayLoad<float, float>> SqrtTestPayLoad;
+
+    // clang-format off
+    const SqrtTestPayLoad TestData
+    {
+        {4.0f, 2.0f},
+        {3.0f, 1.7f, 0.1f},
+        {3.0f, 1.73f, 0.01f},
+        {3.0f, 1.73205f, 1.e-5f},
+    };
+    //clang-format on
+
+    for (const auto Data : TestData)
+    {
+        const bool IsEqual = FMath::IsNearlyEqual(FMath::Sqrt(Data.TestValue), Data.ExpectedValue, Data.Tolerance);
+        TestTrueExpr(IsEqual);
+    }
+
+    return true;
+}
+
+bool FMathSin::RunTest(const FString& Parameters)
+{
+    AddInfo("Sin func testing");
+
+    typedef float Degrees;
+    typedef TArray<TestPayLoad<Degrees, float>> SinTestPayLoad;
+
+    // clang-format off
+    const SinTestPayLoad TestData
+    {
+        {Degrees{0.00f}, 0.0f},
+        {Degrees{30.0f}, 0.5f},
+        {Degrees{45.0f}, 0.707f},
+        {Degrees{60.0f}, 0.866f},
+        {Degrees{90.0f}, 1.0f},
+    };
+    //clang-format on
+
+    for (const auto Data : TestData)
+    {
+        const float Rad = FMath::DegreesToRadians(Data.TestValue);
+        TestTrueExpr(FMath::IsNearlyEqual(FMath::Sin(Rad), Data.ExpectedValue, 0.001f));
+    }
+
     return true;
 }
 
